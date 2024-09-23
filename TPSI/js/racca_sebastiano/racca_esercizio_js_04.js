ae46a4073f07c9ba6616033143d6047678a15155
Sebastiano <racca.sebastiano.7805@gmail.com>
2024-09-23 13:02:44 +0200

    aggiunto esercizio 4

---
+++ TPSI/js/racca_sebastiano/racca_esercizio_js_04.js
@@ -0,0 +1,35 @@
+[...document.getElementsByName('h1')].forEach(el => {
+    el.style.color = 'red';
+});
+
+[...document.getElementsByName('h2')].forEach(el => {
+    el.style.color = 'blue';
+    el.style.backgroundColor = 'yellow';
+    el.style.fontSize = '40px';
+    el.innerText = el.innerText.toUpperCase();
+});
+
+[...document.getElementsByName('p')].forEach((el, idx) => {
+    switch (idx) {
+        case 0:
+        case 4:
+            el.style.color = 'red';
+            break;
+        case 2:
+        case 5:
+        case 8:
+            el.style.color = 'lightgreen';
+            break;
+        case 3:
+            el.style.color = "lightblue";
+            el.innerText = el.innerText.toUpperCase();
+            break;
+        case 6:
+            el.innerText = el.innerText.toUpperCase();
+            break;
+        case 7:
+            el.style.fontStyle = 'italic';
+            break;
+
+    }
+});
