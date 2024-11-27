c174b4c4cf48dbc2e0e556c196e77ad13d4bd4c6
Cavallo-Davide <davide.cavallo@istitutoagnelli.it>
2024-11-27 15:43:58 +0100

    Consegna esericzi dal 59 al 68

---
+++ TPSI/js/cavallo_davide/cavallo_esercizi_tdd/esercizio_js_62/es_62.js
@@ -0,0 +1,6 @@
+export const unique_random_numbers = (n,m) => {
+    let set = new Set();
+    
+    while (set.size < n) set.add(Math.floor(Math.random() * m) + 1);
+    return Array.from(set).sort((a, b) => b - a);
+}
