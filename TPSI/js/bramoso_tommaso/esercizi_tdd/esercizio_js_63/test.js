027904c19b277022eca026e734e78471bdeb9f12
Tommaso Bramoso <tommaso.bramoso@istitutoagnelli.it>
2024-11-27 17:29:25 +0100

    es_tdd 59-68

--- TPSI/js/bramoso_tommaso/esercizi_tdd/esercizio_js_63/test.js
+++ TPSI/js/bramoso_tommaso/esercizi_tdd/esercizio_js_63/test.js
@@ -15,21 +15,6 @@ describe("ESERCIZIO 63", () => {
     expect(unique_characters("supercalifragilistichespiralidoso")).toBe("supercalifgthdo");
   });
   test("TEST 02", () => {
-    const wordlist1 = fs.readFileSync(wordlist_path.concat("wordlist1.txt"), "utf-8").split("\n");
-    const result = JSON.parse(fs.readFileSync(base_path.concat("wordlist1_result.txt"), "utf-8"));
-    expect(wordlist1.map(x => unique_characters(x))).toStrictEqual(result)
-  });
-  test("TEST 03", () => {
-    const wordlist2 = fs.readFileSync(wordlist_path.concat("wordlist2.txt"), "utf-8").split("\n");
-    const result = JSON.parse(fs.readFileSync(base_path.concat("wordlist2_result.txt"), "utf-8"));
-    expect(wordlist2.map(x => unique_characters(x))).toStrictEqual(result)
-  });
-  test("TEST 04", () => {
-    const wordlist3 = fs.readFileSync(wordlist_path.concat("wordlist3.txt"), "utf-8").split("\n");
-    const result = JSON.parse(fs.readFileSync(base_path.concat("wordlist3_result.txt"), "utf-8"));
-    expect(wordlist3.map(x => unique_characters(x))).toStrictEqual(result)
-  });
-  test("TEST 05", () => {
     expect(unique_characters("Linus Benedict Torvalds")).toBe("Linus BedctTorval");
   });
 });
--- TPSI/js/bramoso_tommaso/esercizi_tdd/esercizio_js_64/test.js
+++ TPSI/js/bramoso_tommaso/esercizi_tdd/esercizio_js_64/test.js
@@ -22,24 +22,6 @@ describe("ESERCIZIO 64", () => {
     );
   });
   test("TEST 02", () => {
-    const wordlist1 = fs.readFileSync(wordlist_path.concat("wordlist1.txt"), "utf-8").split("\n");
-    const solutions = wordlist1.map(x => count_vocals(x));
-    const real_solution = JSON.parse(fs.readFileSync(base_path.concat("wordlist1_result.txt"), "utf-8")).map((a) => new Map(JSON.parse(JSON.stringify(a))));
-    expect(solutions).toStrictEqual(real_solution)
-  });
-  test("TEST 03", () => {
-    const wordlist1 = fs.readFileSync(wordlist_path.concat("wordlist2.txt"), "utf-8").split("\n");
-    const solutions = wordlist1.map(x => count_vocals(x));
-    const real_solution = JSON.parse(fs.readFileSync(base_path.concat("wordlist2_result.txt"), "utf-8")).map((a) => new Map(JSON.parse(JSON.stringify(a))));
-    expect(solutions).toStrictEqual(real_solution)
-  });
-  test("TEST 04", () => {
-    const wordlist1 = fs.readFileSync(wordlist_path.concat("wordlist3.txt"), "utf-8").split("\n");
-    const solutions = wordlist1.map(x => count_vocals(x));
-    const real_solution = JSON.parse(fs.readFileSync(base_path.concat("wordlist3_result.txt"), "utf-8")).map((a) => new Map(JSON.parse(JSON.stringify(a))));
-    expect(solutions).toStrictEqual(real_solution)
-  });
-  test("TEST 05", () => {
     expect(count_vocals("Richard Stallman")).toStrictEqual(new Map([['i', 1], ['a', 3]]));
   });
 });
