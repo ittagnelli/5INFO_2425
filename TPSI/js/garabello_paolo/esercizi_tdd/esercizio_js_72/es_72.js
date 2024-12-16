ce2bff8e4fb6212bbb21219f97717d6a0947d227
Paolo Garabello <paolo.garabello06@gmail.com>
2024-12-16 12:37:30 +0100

    SetUp per todos-web

--- TPSI/js/garabello_paolo/esercizi_tdd/esercizio_js_72/es_72.js
+++ TPSI/js/garabello_paolo/esercizi_tdd/esercizio_js_72/es_72.js
@@ -13,5 +13,7 @@ export class Docente {
 }
 
 export function confrontaOBJ(obj1, obj2) {
-    return ((obj1 instanceof Studente && obj2 instanceof Studente) || (obj1 instanceof Docente && obj2 instanceof Docente)) && (JSON.stringify(obj1) == JSON.stringify(obj2));
+    return ((obj1 instanceof Studente && obj2 instanceof Studente) || 
+            (obj1 instanceof Docente && obj2 instanceof Docente)) && 
+            (JSON.stringify(obj1) == JSON.stringify(obj2));
 }
