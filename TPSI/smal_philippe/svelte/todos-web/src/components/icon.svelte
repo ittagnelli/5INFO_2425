76b53d408b8715f376188368105a864e5bf125e1
Smal-Philippe <philippealainmaria.smal@istitutoagnelli.it>
2025-01-27 13:39:44 +0100

    Consegno todos-web

---
+++ TPSI/smal_philippe/svelte/todos-web/src/components/icon.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+    <link rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
+</svelte:head>
+
+<script>
+    export let nome
+</script>
+
+<span class="material-icons-outlined icon">
+    {nome}
+</span>
+
+<style>
+    .icon {
+        font-size: 32px;
+    }
+
+    .icon:hover {
+        cursor: pointer;
+    }
+</style>
