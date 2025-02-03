e5bdd7915fada9cee8c47b9f99d61c09ddf9bfd8
= <=>
2025-02-03 13:40:23 +0100

    catalano todos web

---
+++ TPSI/catalano_leonardo/Svelte/ToDoSWeb-App/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<main>
+    <ToList />
+</main>
+
+<style>
+    main {
+        border: 3px solid red;
+        width:  60%;
+        height: 90vh;
+        margin: auto;
+    }
+</style>
+
+<script>
+    import ToList from "../components/todo_list.svelte";
+</script>
