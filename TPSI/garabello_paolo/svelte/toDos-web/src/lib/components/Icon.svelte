bd3dfb2554e74f8f928a82f84ae73286403fd80c
Paolo Garabello <paolo.garabello06@gmail.com>
2025-02-03 13:40:50 +0100

    Updated ToDo-Web

--- TPSI/garabello_paolo/svelte/toDos-web/src/lib/components/Icon.svelte
+++ TPSI/garabello_paolo/svelte/toDos-web/src/lib/components/Icon.svelte
@@ -7,15 +7,17 @@
 <script>
     export let handler = () => {};
     export let name;
+    export let color  = "green";
 </script>
 
-<button class="material-icons-outlined icon" onclick="{handler}">
-     {name}
+<button class="material-icons-outlined icon" onclick="{handler}" style="--color:{color}">
+    {name}
 </button>
 
 <style>
     .icon {
         font-size: 32px;
+        color: var(--color);
     }
     .icon:hover {
         cursor: pointer;
--- TPSI/garabello_paolo/svelte/toDos-web/src/lib/components/Priority.svelte
+++ TPSI/garabello_paolo/svelte/toDos-web/src/lib/components/Priority.svelte
@@ -1,9 +1,28 @@
-<span class="ALTA">
-    ALTA
-</span>
+<script>
+    export let priority = 3;
+    export let disabled = false;
+
+    const prio2str = (priority) => {
+        let map = {
+            3: 'ALTA',
+            2: 'MEDIA',
+            1: 'BASSA'
+        }
+        return map[priority];
+    }
+
+    const changePrio = () => {
+        if(!disabled)
+            priority = ((priority + 1) % 3) + 1
+    }
+</script>
+
+<button class="{disabled ? 'DISABILITATP' : prio2str(priority)}" onclick="{changePrio}">
+    {prio2str(priority)}
+</button>
 
 <style>
-    span {
+    button {
         display: inline-block;
         border-radius: 5px;
         color: white;
@@ -12,11 +31,22 @@
         height: 30px;
     }
 
-    span:hover {
+    button:hover {
         cursor: pointer;
     }
 
     .ALTA {
         background-color: red;
+    }
+
+    .MEDIA {
+        background-color: orange;
+    }
+
+    .BASSA {
+        background-color: green;
+    }
+    .DISABILITATO {
+        background-color: grey;
     }
 </style>
