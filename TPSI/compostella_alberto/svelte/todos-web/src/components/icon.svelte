f938fa5c41c84487078be22335d7226a74c68d94
Compostella-Alberto <alberto.compostella@istitutoagnelli.it>
2025-02-17 13:08:08 +0100

    Consegno esercizi

---
+++ TPSI/compostella_alberto/svelte/todos-web/src/components/icon.svelte
@@ -0,0 +1,25 @@
+<svelte:head>
+    <link rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
+</svelte:head>
+
+<script>
+    export let nome
+    export let handler = () => {};
+    export let color = "red";
+</script>
+
+<span class="material-icons-outlined icon" style="--color:{color};"  on:click={handler}>
+    {nome}
+</span>
+
+<style>
+    .icon {
+        color: var(--color);
+        font-size: 32px;
+    }
+
+    .icon:hover {
+        cursor: pointer;
+    }
+</style>
